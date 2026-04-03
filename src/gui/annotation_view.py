2d57eb9c571cb44ed06c50286b3ed620eefcfccf
Jan Lattenkamp <145198416+LachsImZweifel@users.noreply.github.com>
2026-04-03 15:58:35 +0200

    mouse wheel zoom implemented

--- src/gui/annotation_view.py
+++ src/gui/annotation_view.py
@@ -8,10 +8,14 @@ class AnnotationView(QGraphicsView):
 
     def __init__(self, scene, parent=None):
         super().__init__(scene, parent)
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+
         self._scene = scene
         self._frame_obj = None
         self._click_task = self._zoom
 
+    ####### Custom methods #######
     def set_image(self, image_data):
         q_img = QImage(
             image_data.data,
@@ -25,13 +29,10 @@ class AnnotationView(QGraphicsView):
         self.frame_obj = self._scene.addPixmap(pixmap)
         self.update_image_scale(self.frame_obj)
 
-    def _zoom(self, coordinates: QPointF, factor=5):
+    def _zoom(self, coordinates: QPointF, factor=10):
         self.scale(factor, factor)
         self.centerOn(coordinates)
         self._click_task = self._emit_click
-
-    def _emit_click(self, coordinates: QPointF):
-        self.point_clicked.emit(coordinates)
 
     def update_image_scale(self, frame_obj):
         if frame_obj:
@@ -40,8 +41,22 @@ class AnnotationView(QGraphicsView):
                 Qt.AspectRatioMode.KeepAspectRatio
             )
 
+    ####### Signal handlers #######
+    def _emit_click(self, coordinates: QPointF):
+        self.point_clicked.emit(coordinates)
+
+    ####### Event handlers #######
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
             self._click_task(self.mapToScene(event.pos()))
 
         super().mousePressEvent(event)
+
+    def wheelEvent(self, event):
+        zoom_in_factor = 1.25
+        zoom_out_factor = 1 / zoom_in_factor
+
+        if event.angleDelta().y() > 0:
+            self.scale(zoom_in_factor, zoom_in_factor)
+        else:
+            self.scale(zoom_out_factor, zoom_out_factor)
